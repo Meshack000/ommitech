de2ed76a31ae6f62b8c014f171d0f1c3e910d34b
Meshack000 <oppongmeshack54@gmail.com>
2026-04-12 13:18:29 -0700

    Improve SEO meta tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,32 +6,10 @@ function Footer() {
  {
    
           <>
-              <style>{`
-                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-            
-                * {
-                    font-family: 'Poppins', sans-serif;
-                }
-            `}</style>
+              
 
               <footer className="flex flex-col bg-slate-50 items-center justify-around w-full py-16 text-sm text-gray-800/70">
-                  <div className="flex items-center gap-8">
-                      <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
-                          Home
-                      </a>
-                      <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
-                          About
-                      </a>
-                      <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
-                          Services
-                      </a>
-                      <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
-                          Contact
-                      </a>
-                      <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
-                          Help
-                      </a>
-                  </div>
+                 
                   <div className="flex items-center gap-4 mt-8 text-indigo-500">
                       <a href="#" className="hover:-translate-y-0.5 transition-all duration-300">
                           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
